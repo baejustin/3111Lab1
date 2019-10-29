bc3cf2af831dce508f5e436e21f2cf76e12585df
baeju <baeju@10.89.188.63>
2019-10-29 19:51:40 +0800

    Some Change made

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -5,6 +5,8 @@ public class Library {
 	
 	public static void main(String args[])
 	{
+		Library lib = new Library();
+		if(lib.someLibraryMethod())
 		System.out.println("When there is a file, commit and push");
 	}
     public boolean someLibraryMethod() {
